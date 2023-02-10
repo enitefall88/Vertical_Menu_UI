1bbd06e242e6146b0776340c417493cb8b9529a4
Anton Durandin <enitefall81@gmail.com>
2023-02-10 13:50:23 -0800

    display a list of items

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 function App() {
   let list = ["first item", "second item", "third item", "fourth item"]
 
+  let query = (new URL(document.location)).searchParams
+  let page = Number(query.get("p")) || 1 // current page
+
   return (
     <div className="App">
-      <ul>
-        {list.map(element => listElement(element))}
+      <ul className="list-group">
+        {list.map((element, index) => listElement({element, index, page}))}
+
       </ul>
     </div>
   );
 }
 
-function listElement(element) {
+function listElement({element, index, page}) {
+
+
   return <li>
-    <a >
+    <a key={index} href={`?page=${index}`} className={`list-group-item list-group-item-action ${index == page ? "active" : null}`} >
     {element}
     </a>
   </li>

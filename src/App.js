7c678f05fba75e6ed49b66bdab0a52f2b97c33c5
Anton Durandin <enitefall81@gmail.com>
2023-02-10 13:16:36 -0800

    display a list of items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  let list = ["first item", "second item", "third item", "fourth item"]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {list.map(element => listElement(element))}
+      </ul>
     </div>
   );
+}
+
+function listElement(element) {
+  return <li>
+    <a >
+    {element}
+    </a>
+  </li>
 }
 
 export default App;
